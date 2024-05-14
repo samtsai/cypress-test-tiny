53aa1091033cc7f2e09ae15049c1a79a20647494
Sam Tsai <satsai@cisco.com>
2024-05-14 13:58:13 -0400

    Basic window confirm and alert

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,9 @@
 /// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("page", () => {
+  it("works", () => {
+    cy.visit("http://localhost:8080");
+
+    cy.contains("Confirm").click();
+    cy.contains("Alert").click();
+  });
+});
