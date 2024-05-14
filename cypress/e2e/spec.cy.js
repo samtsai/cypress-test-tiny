89b9f32d55ee558ff8ccab21b5fe4275a766e1b8
Sam Tsai <satsai@cisco.com>
2024-05-14 14:37:56 -0400

    Add iframe example

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -18,4 +18,31 @@ describe("page", () => {
       .should("have.been.calledOnce")
       .and("be.calledWithExactly", "ask user");
   });
+
+  it("works in iFrame", () => {
+    cy.visit("http://localhost:8080", {
+      onBeforeLoad: (contentWindow) => {
+        // contentWindow is the remote page's window object
+        cy.stub(contentWindow, "alert").as("textareaWindowAlert");
+        cy.stub(contentWindow, "confirm").as("windowConfirm");
+      },
+    });
+
+    cy.get("iframe").then(($iframe) => {
+      const $body = $iframe.contents().find("body");
+      const $win = $iframe[0].contentWindow;
+      cy.stub($win, "confirm")
+        .callsFake(() => true)
+        .as("windowConfirm");
+
+      cy.wrap($body)
+        .contains("Confirm in iFrame")
+        .click()
+        .should(function () {
+          expect(this.windowConfirm).to.be.calledWith(
+            "Cypress should auto confirm from iFrame"
+          );
+        });
+    });
+  });
 });
