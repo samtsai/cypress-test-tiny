77b4d71ecefcb7e4eee1a7aa56050d6ca90ba50e
Sam Tsai <satsai@cisco.com>
2024-05-14 14:22:36 -0400

    Add beforeunload use case

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,9 +1,21 @@
 /// <reference types="cypress" />
+
+import { stubUnload } from "../support/e2e";
+
 describe("page", () => {
+  beforeEach(() => {
+    stubUnload();
+  });
+
   it("works", () => {
     cy.visit("http://localhost:8080");
 
     cy.contains("Confirm").click();
     cy.contains("Alert").click();
+
+    cy.reload();
+    cy.get("@returnValue")
+      .should("have.been.calledOnce")
+      .and("be.calledWithExactly", "ask user");
   });
 });
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -14,7 +14,46 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import "./commands";
 
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
+const stubUnload = () => {
+  const returnValueStub = cy.stub().as("returnValue");
+
+  cy.on("window:before:load", (win) => {
+    let userCallback, ourCallback;
+    Object.defineProperty(win, "onbeforeunload", {
+      get() {
+        return ourCallback;
+      },
+      set(cb) {
+        userCallback = cb;
+        console.log("user callback", cb);
+
+        ourCallback = (e) => {
+          console.log("proxy beforeunload event", e);
+
+          // prevent the application code from assigning value
+          Object.defineProperty(e, "returnValue", {
+            get() {
+              return "";
+            },
+            set: returnValueStub,
+          });
+
+          const result = userCallback(e);
+          return result;
+        };
+
+        // https://developer.mozilla.org/en-US/docs/Web/API/Window/beforeunload_event
+        win.addEventListener("beforeunload", ourCallback);
+      },
+    });
+  });
+};
+
+const stubAlert = (contentWindow) => {
+  cy.stub(contentWindow, "alert").as("windowAlert");
+  return contentWindow;
+};
+
+export { stubAlert, stubUnload };
